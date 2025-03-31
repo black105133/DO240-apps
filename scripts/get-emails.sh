81acbe7e3afcb48f61643d7a0ceee98e229befdb
black105133 <34879427+black105133@users.noreply.github.com>
2025-03-31 15:55:22 +0800

    Update get-emails.sh

--- scripts/get-emails.sh
+++ scripts/get-emails.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-T_SCALE_PROJECT="3scale-project"
+T_SCALE_PROJECT="3scale"
 OCP_USER="admin"
-OCP_PASS="4Z1MhSrM"
-OCP_URL="https://api.restricted-05.bttech.ocp:6443"
+OCP_PASS="ja3oI2U6"
+OCP_URL="https://api.sno.bttech.ocp:6443"
 
 oc login -u "${OCP_USER}" -p "${OCP_PASS}" "${OCP_URL}"
 POD_NAME=$(oc get pods --selector=app=smtp -o=jsonpath='{.items[0].metadata.name}' -n $T_SCALE_PROJECT)
